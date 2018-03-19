50c5deabfa08f7f80a239b55ea5250b817ccc565
Miguel Louzao <luismiguel.louzao.gonzalez@gmail.com>
2018-03-19 11:22:08 +0100

    Add Truffle framework, create project structure and first contract version

---
+++ contracts/Ownable.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.11;
+
+contract Ownable {
+  address public owner = msg.sender;
+
+  /// @notice check if the caller is the owner of the contract
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /// @notice change the owner of the contract
+  /// @param _newOwner the address of the new owner of the contract.
+  function changeOwner(address _newOwner) public onlyOwner{
+    require( _newOwner != 0x0);
+    owner = _newOwner;
+  }
+  
+}
